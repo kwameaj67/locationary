0f6bbb481349ca3184ffac2baa25fbf3f21b6143
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-12 18:44:51 +0000

    complete bottomBar

---
+++ src/components/BottomBar/BottomBar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import bottomBarRoutes from './BottomBarData'
+import './bottomBar.css'
+
+
+
+function BottomBarComponent() {
+    return (
+        <div className="bottom_bar">
+            <ul>
+                {
+                    bottomBarRoutes.map((value, key) => {
+                        return (
+                            <div className="bar_row" >
+                                <Link to={value.path}>
+                                    <li>
+                                        <div key={key} className="item">
+                                            <div className="icon">{value.icon}</div>
+                                            <div><p className="bar_title">{value.title}</p></div>
+                                        </div>
+                                    </li>
+
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+
+}
+
+export default BottomBarComponent
+
+
+
+
+
+// type rafce for boilerPlate 
