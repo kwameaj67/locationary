3e8402adc1bb470da5b32491b895aca92bcc6ad7
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-12 18:36:41 +0000

    modified main App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import AppRouter from './Router/Router'
+import MainRouter from './Router/Router'
+import { BrowserRouter as Router, } from 'react-router-dom'
+import BottomBar from './components/BottomBar/BottomBar'
+
 
 function App() {
   return (
-    <div className="App">
-      <AppRouter/>
+    <div className="app_wrapper">
+      <div className="app_container">
+        <Router>
+          <div className="main_area">
+            <MainRouter/>
+          </div>
+          <div className="bottom_bar_area">
+            <BottomBar />
+          </div>
+        </Router>
+      </div>
     </div>
   );
 }
