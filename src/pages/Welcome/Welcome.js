63382a115f95f608c53730eae98d454f059d908e
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-14 21:25:13 +0000

    added welcome page

---
+++ src/pages/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './welcome.css'
+
+const WelcomePage = () => {
+    return (
+        <div className="welcome_container">
+            <h1>Welcome to myLocations</h1>
+            <p>An app that organizes categories and locations</p>
+        </div>
+    )
+}
+
+export default WelcomePage
