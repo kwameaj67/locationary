c1f1791719ccc8949e8693ecd8879324dbcdb7d1
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-14 21:26:26 +0000

    add new routes

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,7 +1,8 @@
-import { Switch, Route,useLocation } from 'react-router-dom'
+import { Switch, Route, useLocation } from 'react-router-dom'
 import routes from '../components/BottomBar/BottomBarData'
 import { CSSTransition } from "react-transition-group";
 import AllLocationPage from '../pages/AllLocation/AllLocations'
+import WelcomePage from '../pages/Welcome/Welcome'
 
 
 function AppRouter() {
@@ -12,19 +13,25 @@ function AppRouter() {
             classNames="fade"
             timeout={500}
         >
-            <Switch location={location}>
-                {routes.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        children={<route.main />}
-                    />
-                ))}
-                <Switch>
-                    <Route path="/viewLocations">
-                        <AllLocationPage/>
-                    </Route>
+            <Switch>
+                <Switch location={location}>
+                    {routes.map((route, index) => (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            exact={route.exact}
+                            children={<route.main />}
+                        />
+
+                    ))}
+                    <Switch>
+                        <Route path="/viewLocations">
+                            <AllLocationPage />
+                        </Route>
+                        <Route path="/">
+                            <WelcomePage />
+                        </Route>
+                    </Switch>
                 </Switch>
             </Switch>
         </CSSTransition>
