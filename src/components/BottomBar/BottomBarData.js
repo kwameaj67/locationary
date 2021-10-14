db5134df375473a0cc45acfdf9e3ff34d72941f5
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-14 22:56:16 +0000

    changes bottomBar

--- src/components/BottomBar/BottomBarData.js
+++ src/components/BottomBar/BottomBarData.js
@@ -8,14 +8,14 @@ const tabs = [
       path: "/category",
       exact: true,
       title:"Category",
-      icon:<MdCategory size={20} color="#55575b"/>,
+      icon:<MdCategory size={20} color="white"/>,
       main: () => <Category/>
     },
     {
       path: "/location",
       exact: true,
       title:"Location",
-      icon:<MdLocationOn size={20} color="#55575b"/>,
+      icon:<MdLocationOn size={20} color="white"/>,
       main: () => <Location/>
     }
 ]
