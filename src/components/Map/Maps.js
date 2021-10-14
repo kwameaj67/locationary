fc60451c7eb51a31a63dcfc8cef1e5b8c71c1598
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-14 01:55:45 +0000

    create map component

---
+++ src/components/Map/Maps.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { GoogleMap,useLoadScript } from '@react-google-maps/api';
+import { MAPS_API } from '../../utils/config'
+import './maps.css'
+import { mapStyles } from './mapStyles'
+
+
+const Maps = (props) => {
+    const containerStyle = {
+        width: '50vw',
+        height: '50vh'
+    };
+    // const center = {
+    //     lat: -3.745,
+    //     lng: -38.523
+    //   };
+    const options = {
+        styles : mapStyles,
+        zoomControl:true,
+        disableDefaultUI:true
+    }
+    const { isLoaded,loadError } = useLoadScript({
+        googleMapsApiKey:MAPS_API,
+
+    })
+    if (loadError) return <div className="loading_text">Error loading Google Maps🥶</div>
+    if (!isLoaded) return <div className="loading_text">Loading maps.. Please wait</div>
+    return (
+        // <LoadScript googleMapsApiKey={MAPS_API}>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                zoom={10}
+                center={props.center}
+                options={options}
+                onClick={props.onClickAddress}
+                >
+                  {props.children}
+            </GoogleMap>
+        // </LoadScript>
+    )
+}
+
+export default Maps
