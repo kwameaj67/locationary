7615e7097d1b39b6e18f9f0c7c8300e73b80d5a8
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-14 01:54:10 +0000

    created category reducer slice

---
+++ src/redux/slices/categorySlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// slices controls, defines reducers
+const categorySlice  = createSlice({
+    name:"categories",
+    initialState: [
+        {id:1,name:"Plants",completed:false},
+        {id:2,name:"Fuego",completed:false},
+        {id:3,name:"One Dance",completed:false},
+    ],
+    reducers:{
+        addCategoryAction: (state, action) => {
+            const newCategory = {
+                id:Date.now(),
+                name:action.payload.name,
+                completed:true,
+            }
+            state.push(newCategory);
+        },
+        removeCategoryAction: (state, action) =>{
+           const removeItem = state.filter((item) => item.id !== action.payload.id)
+           return removeItem
+        },
+        editCategoryAction: (state, action) =>{
+            // finds a particular item index in entire state
+            const index = state.findIndex((item) => item.id === action.payload.id)  
+            state[index].name = action.payload.name  //sets whatever value a componenet passed to update the name item in state
+        }
+    }
+})
+// we export slices as actions so we can access its logic
+export const { addCategoryAction,removeCategoryAction,editCategoryAction } = categorySlice.actions
+// we do this to add to the store
+export default categorySlice.reducer
