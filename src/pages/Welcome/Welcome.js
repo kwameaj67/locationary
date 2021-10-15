ea8e70dd65aec208fcd33a55bb3f0af4258355b5
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-15 00:31:22 +0000

    changes

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -8,7 +8,7 @@ const WelcomePage = () => {
     return (
         <div className="welcome_container">
             <h1>Welcome to myLocations</h1>
-            <p>An app that organizes categorizes name locations</p>
+            <p>An app that organizes categorizes name locations.</p>
         </div>
     )
 }
