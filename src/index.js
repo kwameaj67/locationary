5806635413524017a0cc5e8686cf226e73ea4437
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-15 00:15:01 +0000

    changes store name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  reduxStore from './redux/store/Store'
+import  reduxStore from './redux/store/store'
 import { Provider }  from 'react-redux'
 
 
