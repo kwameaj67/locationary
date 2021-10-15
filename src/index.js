2082a4980ac719bb0a5ce342283716f368fbe35c
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-15 00:28:10 +0000

    changes store folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import  reduxStore from './redux/store/store'
+import  reduxStore from './redux/Store/store'
 import { Provider }  from 'react-redux'
 
 
