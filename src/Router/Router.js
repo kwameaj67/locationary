2f89ee6ac30a35684e134d9714d4fd794e4b2478
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-26 01:28:38 +0000

    modified router

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,42 +1,35 @@
 import { Switch, Route, useLocation } from 'react-router-dom'
 import routes from '../components/BottomBar/BottomBarData'
-import { CSSTransition } from "react-transition-group";
-import { AllLocationsPage,WelcomePage,ErrorPage } from '../pages/index'
+import { AllLocationsPage, WelcomePage, ErrorPage } from '../pages/index'
 
 
 function AppRouter() {
     const location = useLocation()
     return (
-        <CSSTransition
-            key={location.key}
-            classNames="fade"
-            timeout={500}
-        >
-            <Switch>
-                <Switch location={location}>
-                    {routes.map((route, index) => (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            exact={route.exact}
-                            children={<route.main />}
-                        />
-                    ))}
-                    <Switch>
-                        <Route exact path="/viewLocations">
-                            <AllLocationsPage />
-                        </Route>
-                       
-                        <Route exact path="/">
-                            <WelcomePage />
-                        </Route>
-                        <Route exact path="/*">
-                            <ErrorPage/>
-                        </Route>
-                    </Switch>
-                </Switch>
+        <Switch>
+            <Switch location={location}>
+                {routes.map((route, index) => (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        exact={route.exact}
+                        children={<route.main />}
+                    />
+                ))}
+                {/* <Switch> */}
+                    <Route exact path="/viewLocations">
+                        <AllLocationsPage />
+                    </Route>
+
+                    <Route exact path="/">
+                        <WelcomePage />
+                    </Route>
+                    <Route exact path="/*">
+                        <ErrorPage />
+                    </Route>
+                {/* </Switch> */}
             </Switch>
-        </CSSTransition>
+        </Switch>
     )
 }
 export default AppRouter
