d16f280e4f304db92a255e720545f7384a1e99ad
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-01-28 10:41:15 +0000

    fix router animation

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,35 +1,41 @@
+import React from 'react'
 import { Switch, Route, useLocation } from 'react-router-dom'
 import routes from '../components/BottomBar/BottomBarData'
 import { AllLocationsPage, WelcomePage, ErrorPage } from '../pages/index'
+import { TransitionGroup,CSSTransition } from 'react-transition-group'
+import './router.css'
 
 
 function AppRouter() {
-    const location = useLocation()
+    const location = useLocation();
     return (
-        <Switch>
-            <Switch location={location}>
-                {routes.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        children={<route.main />}
-                    />
-                ))}
-                {/* <Switch> */}
-                    <Route exact path="/viewLocations">
-                        <AllLocationsPage />
-                    </Route>
+        <React.Suspense fallback={<p></p>}>
+            <TransitionGroup>
+                <CSSTransition timeout={450} classNames="fade" key={location.key}>
+                    <Switch location={location}>
+                        {routes.map((route, index) => (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                exact={route.exact}
+                                children={<route.main />}
+                            />
+                        ))}
+                        <Route path="/viewLocations">
+                            <AllLocationsPage />
+                        </Route>
 
-                    <Route exact path="/">
-                        <WelcomePage />
-                    </Route>
-                    <Route exact path="/*">
-                        <ErrorPage />
-                    </Route>
-                {/* </Switch> */}
-            </Switch>
-        </Switch>
+                        <Route exact path="/">
+                            <WelcomePage />
+                        </Route>
+                        <Route path="*">
+                            <ErrorPage />
+                        </Route>
+                    </Switch>
+                </CSSTransition>
+            </TransitionGroup>
+        </React.Suspense>
+
     )
 }
 export default AppRouter
